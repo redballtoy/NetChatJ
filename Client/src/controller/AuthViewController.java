ce591ebd0f7fcee178b4d10b4176efe8bc0b89be
Dmitry Sorokin <redballtoy@gmail.com>
2020-12-08 12:45:05 +0300

    AuthViewController

--- Client/src/controller/AuthViewController.java
+++ Client/src/controller/AuthViewController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import models.Client;
 import net.Network;
+import util.AlertError;
 
 public class AuthViewController {
 
@@ -15,6 +16,29 @@ public class AuthViewController {
     //добавляем Network что бы через него идентифицироваться
     private Network network;
     private Client networkClient;
+
+    @FXML
+    //эту функцию для кнопки которая будет вызываться при нажатии задали в
+    //SceneBuilder
+    public void checkAuth() {
+        String login = loginField.getText();
+        String password = passwordField.getText();
+
+        //Если поля не пустые, передаем данные не Network а в свою очередь на сервер
+        //там они будут проверены на валидность
+        if (login.isEmpty() || password.isEmpty()) {
+            new AlertError().alertGo("EmptyField","Не введены пароль или логин",
+                    "Введите логин и пароль");
+            return;
+        }
+
+        //отправить параметры на авторизацию и вернуть результат
+        //String authErrorMessage = network.sendAuthCommnd(login, password);
+        //
+
+
+    }
+
 
     public void setNetwork(Network network) {
         this.network = network;
